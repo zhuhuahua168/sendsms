d672e3dc0fbaa3b36d886e18a287efb81305c00f
zhuhuahua <zhuhuahua@>
2021-05-28 09:57:36 +0800

    测试通过

--- test.py
+++ test.py
@@ -43,10 +43,11 @@ def power():
 
 def job_func():
     print("job1当前时间1：", datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"))
-
+    time.sleep(124)
+    print("job1当前时间1睡眠2分钟：", datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"))
 def job_2():
     print("job2当前时间22：", datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f"))
 if __name__ == '__main__':
-    scheduler.add_job(job_func, 'interval', minutes=5, start_date='2021-05-26 17:00:00', end_date='2021-05-26 18:00:00')
-    scheduler.add_job(job_2, 'interval', minutes=6, start_date='2021-05-26 17:00:00', end_date='2021-05-26 18:00:00')
+    scheduler.add_job(job_func, 'interval', minutes=1, start_date='2021-05-27 14:00:00', end_date='2021-05-27 17:00:00')
+    scheduler.add_job(job_2, 'interval', minutes=1, start_date='2021-05-27 14:00:00', end_date='2021-05-27 17:00:00')
     scheduler.start()
